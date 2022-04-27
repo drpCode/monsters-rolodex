2e976d46f66dcbc2d6c34271b2fb746b92671ce8
Daniel Ruiz Pastor <daniel.ruiz.pastor@gmail.com>
2022-04-27 18:25:27 +0200

    Part 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import {Component} from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,24 +7,31 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monster1: {
-        name: 'Linda'
-      },
-      monster2: {
-        name: 'Frank'
-      },
-      monster3: {
-        name: 'Jacky'
-      }
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jacky'
+        }
+      ]
+      
     };
   }
 
   render(){
     return (
       <div className="App">
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
+        {
+          this.state.monsters.map(
+            (monster) => {
+              return <h1>{monster.name}</h1>;  
+            } 
+          )
+        }
       </div>
     );
   }
